4d4eef2b1b12e3bcfa146a080a7194ebc4362ffb
droxbrl <rustanovich01@gmail.com>
2022-12-22 19:36:22 +0400

    Добавил функцию get_by_id() в модуль WorkerDB.py. Функция для получения данных из таблицы БД по id.

--- WorkerDB.py
+++ WorkerDB.py
@@ -1,7 +1,7 @@
 """Модуль для работы с SQLite3."""
 
 import sqlite3
-
+from typing import List, Dict
 
 class DataBaseWorker:
     """Класс для подключения и курсора БД SQLite3."""
@@ -41,3 +41,34 @@ class DataBaseWorker:
         """ Очищает текст запроса. """
         self.set_query_text('')
 
+
+def get_by_id(id: (str, int), table_name: str, columns: List[str], column_id_name='id', dbw=None) -> Dict or None:
+    """
+        Возвращает все значения полей из таблицы БД по переданному id.
+        Если записи нет или возникла ошибка - возвращает None.
+    """
+    if dbw is None:
+        dbw = DataBaseWorker()
+    columns_joined = ", ".join(columns)
+
+    if isinstance(id, str):
+        query_text = f'SELECT {columns_joined} FROM {table_name} WHERE {column_id_name} = {repr(id)}'
+    else:
+        query_text = f'SELECT {columns_joined} FROM {table_name} WHERE {column_id_name} = {id}'
+
+    dbw.set_query_text(query_text)
+    try:
+        dbw.execute()
+    except sqlite3.OperationalError:
+        dbw.close()
+        return None
+
+    sql_result = dbw.cursor.fetchone()
+    if sql_result is None:
+        return sql_result
+
+    result = {}
+    for index, column in enumerate(columns):
+        result[column] = sql_result[index]
+
+    return result
