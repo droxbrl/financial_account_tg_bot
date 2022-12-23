2e145666c4ff8a761d5c600d3928da8577fd6315
droxbrl <rustanovich01@gmail.com>
2022-12-23 17:14:28 +0400

    Доработка класса Columns

--- Collections.py
+++ Collections.py
@@ -18,19 +18,38 @@ class Columns:
 
         if isinstance(columns_names, list):
             for column_name in columns_names:
-                self.__columns.append(Column(column_name))
+                self.append(column=column_name)
 
-    def append(self, new_column: str or Column):
+    def append(self, column: str or Column):
         """Добавляет колонку в коллекцию последним элементом."""
-        if isinstance(new_column, str):
-            self.__columns.append(Column(new_column))
-        elif isinstance(new_column, Column):
-            self.__columns.append(new_column)
+        if isinstance(column, str):
+            column = Column(column)
+            if column not in self.get_columns():
+                self.__columns.append(column)
+        elif isinstance(column, Column):
+            if column not in self.get_columns():
+                self.__columns.append(column)
 
-    def delete(self, del_column: str or Column):
-        """Удаляет колонку из коллекции."""
-        # TODO: Реализовать удаление + удаление по индексу ?
-        pass
+    def delete(self, column: str or Column or int):
+        """Удаляет колонку из коллекции по имени, по экземпляру класса Column или по индексу колонки."""
+        if isinstance(column, Column):
+            try:
+                index = self.get_columns().index(column)
+            except ValueError:
+                index = None
+        elif isinstance(column, str):
+            index = self.find(search_column=column)['index']
+        elif isinstance(column, int):
+            index = column
+        else:
+            index = None
+
+        if index is not None:
+            self.__columns.pop(index)
+
+    def delete_all(self):
+        """Удаляет все колонки из коллекции."""
+        self.__columns.clear()
 
     def find(self, search_column: str or Column) -> dict:
         """
@@ -42,8 +61,8 @@ class Columns:
         """
         search_result = self.__get_search_result()
         if isinstance(search_column, str):
-            search_column = search_column.capitalize()
-        for index, column in enumerate(self.__columns):
+            search_column = search_column.capitalize().strip()
+        for index, column in enumerate(self.get_columns()):
             if column == search_column or column.name == search_column:
                 search_result['index'] = index
                 search_result['column'] = column
@@ -54,11 +73,27 @@ class Columns:
 
     def count(self) -> int:
         """Возвращает количество колонок в коллекции."""
-        return len(self.__columns)
+        return len(self.get_columns())
 
     def ubound(self) -> int:
         """Возвращает максимальный индекс колонки в коллекции."""
         return self.count() - 1
+
+    def get_columns(self) -> list[Column]:
+        """Возвращает массив колонок коллекции."""
+        return self.__columns.copy()
+
+    def set_columns(self, columns: list[str or Column]):
+        """ Очищает колонки и устанавливает уникальные колонки из массива.
+            Если после установки колонок, новый массив коллекции пустой, тогда
+            устанавливает значения колонок до установки новых.
+        """
+        current_columns = self.get_columns()
+        self.delete_all()
+        for column in columns:
+            self.append(column=column)
+        if self.count() == 0:
+            self.set_columns(columns=current_columns)
 
     def __get_search_result(self) -> dict:
         """Возвращает пустой словарь для хранения результата поиска в коллекции."""
@@ -67,10 +102,3 @@ class Columns:
             'column': None,
             'exist': False,
         }
-
-
-c_n = ['сумма', 'Итого']
-c_n.append()
-columns_collection = Columns(columns_names=c_n)
-f = columns_collection.find('итого')
-asd = 1
