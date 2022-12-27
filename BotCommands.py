074f27bb996fa88795cc6bee1861719bf9c05d9d
droxbrl <rustanovich01@gmail.com>
2022-12-28 03:16:03 +0400

    bugfix и рефакторинг

--- BotCommands.py
+++ BotCommands.py
@@ -2,7 +2,6 @@
 from telebot import types
 from typing import Optional, List
 from Models import all_categories_from_db, all_currencies_from_db
-from Collections import TreeTable, TreeColumns, TreeRow
 
 
 class BotKeyboard:
@@ -20,11 +19,25 @@ class BotKeyboard:
 class Button:
     """Модель кнопки."""
 
-    def __init__(self, text: str, callback_data: Optional[str] = None,
-                 owner: Optional[str] = None, parent: Optional[str] = None):
+    def __init__(self, text: str, callback_data: Optional[str] = None):
         self.button = types.InlineKeyboardButton(text, callback_data=callback_data)
-        self.owner = owner
-        self.parent = parent
+
+
+class CancelKeyboard(BotKeyboard):
+    """
+        Клавиатура для отмены действий.
+        id: 'cancel',
+        Кнопки: [cancel].
+    """
+
+    def __init__(self):
+        super().__init__(keyboard_id='cancel')
+        self.buttons: List[Button] = []
+        self.buttons.append(Button(text='Отменить', callback_data='cancel'))
+        keyboard = types.InlineKeyboardMarkup()
+        for button in self.buttons:
+            keyboard.add(button.button)
+        self.set_keyboard(keyboard=keyboard)
 
 
 class StartKeyboard(BotKeyboard):
@@ -37,9 +50,12 @@ class StartKeyboard(BotKeyboard):
     def __init__(self):
         super().__init__(keyboard_id='start')
         self.buttons: List[Button] = []
-        self.buttons.append(Button(text='Внести расход', callback_data='add_expenses', owner='categories'))
-        self.buttons.append(Button(text='Внести доход', callback_data='add_income', owner='currencies'))
-        self.buttons.append(Button(text='Отчеты', callback_data='reports', owner='reports'))
+        self.buttons.append(Button(text='Внести расход',
+                                   callback_data='add_expenses'))
+        self.buttons.append(Button(text='Внести доход',
+                                   callback_data='add_income'))
+        self.buttons.append(Button(text='Отчеты',
+                                   callback_data='reports'))
 
         keyboard = types.InlineKeyboardMarkup()
         for button in self.buttons:
@@ -57,11 +73,11 @@ class DateKeyboard(BotKeyboard):
     def __init__(self):
         super().__init__(keyboard_id='date_today')
         self.buttons: List[Button] = []
-        self.buttons.append(Button(text='На сегодня', callback_data='date_today', parent='reports'))
-
+        self.buttons.append(Button(text='На сегодня', callback_data='date_today'))
+        self.buttons.append(Button(text='Отменить', callback_data='cancel'))
         keyboard = types.InlineKeyboardMarkup()
         for button in self.buttons:
-            keyboard.add(button)
+            keyboard.add(button.button)
         self.set_keyboard(keyboard=keyboard)
 
 
@@ -75,11 +91,12 @@ class CommitKeyboard(BotKeyboard):
     def __init__(self):
         super().__init__(keyboard_id='commit')
         self.buttons: List[Button] = []
-        self.buttons.append(Button(text='Подтвердить', callback_data='commit', parent='# User input'))
+        self.buttons.append(Button(text='Подтвердить', callback_data='commit'))
+        self.buttons.append(Button(text='Отменить', callback_data='cancel'))
 
         keyboard = types.InlineKeyboardMarkup()
         for button in self.buttons:
-            keyboard.add(button)
+            keyboard.add(button.button)
         self.set_keyboard(keyboard=keyboard)
 
 
@@ -94,15 +111,17 @@ class ReportsKeyboard(BotKeyboard):
         super().__init__(keyboard_id='reports')
         self.buttons: List[Button] = []
         self.buttons.append(
-            Button(text='Остатки', callback_data='account_balance', parent='reports', owner='date_today'))
+            Button(text='Остатки', callback_data='account_balance'))
         self.buttons.append(
-            Button(text='Расходы', callback_data='expenses', parent='reports', owner='date_today'))
+            Button(text='Расходы', callback_data='expenses'))
         self.buttons.append(
-            Button(text='Доходы', callback_data='income', parent='reports', owner='date_today'))
+            Button(text='Доходы', callback_data='income'))
+        self.buttons.append(
+            Button(text='Отменить', callback_data='cancel'))
 
         keyboard = types.InlineKeyboardMarkup()
         for button in self.buttons:
-            keyboard.add(button)
+            keyboard.add(button.button)
         self.set_keyboard(keyboard=keyboard)
 
 
@@ -121,15 +140,14 @@ class CategoriesKeyboard(BotKeyboard):
             self.buttons.append(
                 Button(
                     text=category.get_name(),
-                    callback_data=f'category_id_{category.get_id()}',
-                    parent='add_income',
-                    owner='currencies'
+                    callback_data=f'category_id_{category.get_id()}'
                 )
             )
+        self.buttons.append(Button(text='Отменить', callback_data='cancel'))
 
         keyboard = types.InlineKeyboardMarkup()
         for button in self.buttons:
-            keyboard.add(button)
+            keyboard.add(button.button)
         self.set_keyboard(keyboard=keyboard)
 
 
@@ -137,7 +155,7 @@ class CurrenciesKeyboard(BotKeyboard):
     """
         Клавиатура для выбора валюты.
         id: 'currencies',
-        Кнопки: [currency_id_{id}].
+        Кнопки: [currency_id_{code}].
     """
 
     def __init__(self):
@@ -149,40 +167,10 @@ class CurrenciesKeyboard(BotKeyboard):
             self.buttons.append(
                 Button(
                     text=currency.get_code(),
-                    callback_data=f'currency_id_{currency.get_id()}',
-                    parent='categories; add_expenses',
-                    owner='# User input'
+                    callback_data=f'currency_id_{currency.get_code()}'
                 )
             )
+        self.buttons.append(Button(text='Отменить', callback_data='cancel'))
         for button in self.buttons:
-            keyboard.add(button)
+            keyboard.add(button.button)
         self.set_keyboard(keyboard=keyboard)
-
-
-def KeyboardMap() -> TreeTable:
-    """Возвращает дерево значений, описывающее взаимосвязь команд бота."""
-    columns = TreeColumns(columns_names=['id_keyboard', 'id_button'])
-    keyboard_map = TreeTable(columns=columns)
-
-    index = 0
-    for keyboard in all_keyboards():
-        for button in keyboard.buttons:
-            row = TreeRow(values=[keyboard.keyboard_id, button.button.callback_data])
-            keyboard_map.rows.append(row=row)
-            keyboard_map.set_owner(row_index=index, owner=button.owner)
-            keyboard_map.set_parent(row_index=index, parent=button.parent)
-            index += 1
-
-    return keyboard_map
-
-
-def all_keyboards() -> List:
-    """Возвращает все клавиатуры."""
-    return [
-        StartKeyboard(),
-        DateKeyboard(),
-        CommitKeyboard(),
-        ReportsKeyboard(),
-        CategoriesKeyboard(),
-        CurrenciesKeyboard(),
-    ]
